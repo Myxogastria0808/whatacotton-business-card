8cc15a7ca0ab62017e3dccf7d7bcfed998974ef3
Myxogastria0808 <r.rstudio.c@gmail.com>
2025-11-13 14:31:42 +0900

    chore: update environment

---
+++ src/components/Card.tsx
@@ -0,0 +1,9 @@
+import styles from "./Card.module.css";
+
+export default function Card() {
+  return (
+    <div class={styles.card}>
+      <h2>RaBit's Business Card</h2>
+    </div>
+  );
+}
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,19 +1,13 @@
 import { Title } from "@solidjs/meta";
-import Counter from "~/components/Counter";
+import { HttpStatusCode } from "@solidjs/start";
+import Card from "~/components/Card";
 
 export default function Home() {
   return (
     <main>
       <Title>Hello World</Title>
-      <h1>Hello world!</h1>
-      <Counter />
-      <p>
-        Visit{" "}
-        <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
-        </a>{" "}
-        to learn how to build SolidStart apps.
-      </p>
+      <HttpStatusCode code={200} />
+      <Card />
     </main>
   );
 }
